9a66cef93e97e5cdea6030c1e7de4f5f9a1238b9
Peter <piotr.szaran@gmail.com>
2020-03-10 19:57:01 +0100

    v23 - Service: UserService updated. Added add() method.

--- src/main/java/pl/szaran/shop_spring/service/UserService.java
+++ src/main/java/pl/szaran/shop_spring/service/UserService.java
@@ -1,9 +1,11 @@
 package pl.szaran.shop_spring.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.szaran.shop_spring.exceptions.ExceptionCode;
 import pl.szaran.shop_spring.exceptions.MyException;
+import pl.szaran.shop_spring.model.User;
 import pl.szaran.shop_spring.model.dto.UserDTO;
 import pl.szaran.shop_spring.repository.UserRepository;
 
@@ -12,6 +14,9 @@ import pl.szaran.shop_spring.repository.UserRepository;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+    private final EmailService emailService;
 
     public boolean doesUserExists(String username) {
         if (username == null) {
@@ -38,6 +43,35 @@ public class UserService {
         }
 
         return userRepository.findByEmail(email).isPresent();
+    }
+
+    public Long add(UserDTO userDTO) {
+        if (userDTO == null) {
+            throw new MyException(ExceptionCode.SERVICE, "USER SERVICE, add() - userDTO is null");
+        }
+
+        User user = ModelMapper.fromUserDTOToUser(userDTO);
+
+        user.setEnabled(false);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        var insertUser = userRepository.save(user);
+
+        System.out.println("Dodano nowego doktora z loginem " + user.getUsername());
+
+        // --------------------------------------------------------------------
+        // PRZYGOTOWANIE EMAIL
+        // --------------------------------------------------------------------
+        var tokenWithDoctor = tokenService.prepareToken(insertUser.getId());
+        System.out.println("Wygenerowano token aktywujacy konto dla " + user.getEmail());
+        var messagePart1 = "Click link to activate account: ";
+        var messagePart2 = "http://localhost:8092/security/account-activation?token=";
+        var messagePart3 = tokenWithDoctor.getToken();
+        var message = messagePart1 + messagePart2 + messagePart3;
+
+        emailService.send(insertUser.getEmail(), "Account Activation", message);
+        System.out.println("Wysłano token do " + user.getEmail());
+
+        return insertUser.getId();
     }
 }
 
