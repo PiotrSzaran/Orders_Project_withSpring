76bae622963b0994e0959950c2641bd87ba52f59
Peter <piotr.szaran@gmail.com>
2020-03-11 20:53:40 +0100

    v38 - Repository: CountryRepository updated

--- src/main/java/pl/szaran/shop_spring/repository/CountryRepository.java
+++ src/main/java/pl/szaran/shop_spring/repository/CountryRepository.java
@@ -3,5 +3,9 @@ package pl.szaran.shop_spring.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.szaran.shop_spring.model.Country;
 
+import java.util.Optional;
+
 public interface CountryRepository extends JpaRepository<Country, Long> {
+
+    Optional<Country> findByName(String name);
 }
