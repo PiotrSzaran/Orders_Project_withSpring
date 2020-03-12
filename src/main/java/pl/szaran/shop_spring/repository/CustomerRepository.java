12e878ec01c71952ff931630ee116abebecf528f
Peter <piotr.szaran@gmail.com>
2020-03-12 20:46:00 +0100

    v49 - Repository: CustomerRepository updated.

--- src/main/java/pl/szaran/shop_spring/repository/CustomerRepository.java
+++ src/main/java/pl/szaran/shop_spring/repository/CustomerRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.szaran.shop_spring.model.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    boolean existsCustomerByAgeAndCountry_NameAndNameAndSurname(Integer age, String countryName, String name, String surname);
 }
