8201d276a603ecdbcb05a4fea1c6b7b6bf994855
Peter <piotr.szaran@gmail.com>
2020-03-12 16:30:05 +0100

    v44 - Repository: TradeRepository updated.

--- src/main/java/pl/szaran/shop_spring/repository/TradeRepository.java
+++ src/main/java/pl/szaran/shop_spring/repository/TradeRepository.java
@@ -3,5 +3,9 @@ package pl.szaran.shop_spring.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.szaran.shop_spring.model.Trade;
 
+import java.util.Optional;
+
 public interface TradeRepository extends JpaRepository<Trade, Long> {
+
+    Optional<Trade> findByName(String name);
 }
